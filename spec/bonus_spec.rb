f8575b6cb12398cb5fd5aee6931a34c874d8c35d
dwane richards <dwanerich@gmail.com>
2019-11-10 00:30:11 +0000

    Automatically backed up by Learn

--- spec/bonus_spec.rb
+++ spec/bonus_spec.rb
@@ -2,8 +2,6 @@ require_relative 'spec_helper'
 require_relative '../lib/bonus.rb'
 
 describe "bonus" do
-  [:montague][:hero][:status] = "dead"
-  [:montague][:heroine][:status] = "dead"
   # remove the 'x' from the 'xit' below to activate this test!
 	it "uses the []= method to change the hero and heroine's status from alive to dead" do
     expect(bonus[:montague][:hero][:status]).to eq("dead")
